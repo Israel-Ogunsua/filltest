1c64191a5c405f2bf4879e5b93046ecb3cc4c10b
Israel-Ogunsua <oluwasunisrael1990@gmail.com>
2021-04-15 22:06:33 -0700

    python added

---
+++ LET.py
@@ -0,0 +1,12 @@
+
+print("hello world" )
+print( "i haven't code in python for month except from coding flask ")
+
+me  = "lazy"
+
+def israel(name, Ft):
+    details= input("What is your todo list: ")
+    if details == Ft :
+        print( "My name is ", name ,"and my to do list is " , Ft)
+
+israel(me, "flask")
--- Readme.md
+++ Readme.md
@@ -1,2 +1,13 @@
 # HI 
 THIS IS THE NEW CALL 
+
+# Dou have something in mind 
+No 
+
+what will i really create 
+
+# Dairy Note
+1 Maybe become an influencer*( but i can't really talk
+2 I am a programer 
+3 I went to get my certification forcus even though i have gotten it (aws certified)
+GOOD!! 
